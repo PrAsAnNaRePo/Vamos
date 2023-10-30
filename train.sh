f70a883e194b350767c80a5dbd63c059eaf221f4
PrAsAnNaRePo <prasannatwenty@gmail.com>
2023-10-30 22:01:04 +0530

    adding training setup

--- train.sh
+++ train.sh
@@ -6,8 +6,9 @@ torchrun --nproc_per_node 4 --nnodes 1 --node_rank 0 --master_addr localhost --m
     --dataset_id "textcap" \
     --output_dir "vamos" \
     --num_train_epochs 2 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 4 \
+    --per_device_eval_batch_size 8 \
     --evaluation_strategy "steps" \
     --save_steps 1000 \
     --save_total_limit 3 \
